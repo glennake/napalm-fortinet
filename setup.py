6007d290e2fe462b9f55c6001b1fad837fe219b8
Glenn <glennake@users.noreply.github.com>
2022-02-21 12:12:03 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""setup.py file."""
+
+import uuid
+
+from setuptools import setup, find_packages
+
+__author__ = 'David Barroso <dbarrosop@dravetech.com>'
+
+with open("requirements.txt", "r") as fs:
+    reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
+
+setup(
+    name="napalm-skeleton",
+    version="0.1.0",
+    packages=find_packages(),
+    author="David Barroso",
+    author_email="dbarrosop@dravetech.com",
+    description="Network Automation and Programmability Abstraction Layer with Multivendor support",
+    classifiers=[
+        'Topic :: Utilities',
+         'Programming Language :: Python',
+         'Programming Language :: Python :: 2',
+         'Programming Language :: Python :: 2.7',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+    ],
+    url="https://github.com/napalm-automation/napalm-skeleton",
+    include_package_data=True,
+    install_requires=reqs,
+)
