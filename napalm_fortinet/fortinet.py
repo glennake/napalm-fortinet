de4e137e271c86d3cf1d0a45ad96ff62e552fb07
Glenn <glennake.1@gmail.com>
2022-02-21 14:15:42 +0000

    initial mandatory functions

---
+++ napalm_fortinet/fortinet.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 Glenn Akester. All rights reserved.
+#
+# The contents of this file are licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+"""
+Napalm driver for Fortinet.
+
+Read https://napalm.readthedocs.io for more information.
+"""
+# from __future__ import print_function
+# from __future__ import unicode_literals
+
+from netmiko import ConnectHandler
+
+from napalm.base import NetworkDriver
+from napalm.base.exceptions import (
+    ConnectionException,
+    SessionLockedException,
+    MergeConfigException,
+    ReplaceConfigException,
+    CommandErrorException,
+)
+
+
+class FortinetDriver(NetworkDriver):
+    """Napalm driver for Fortinet."""
+
+    def __init__(self, hostname, username, password, timeout=60, optional_args=None):
+        """Constructor."""
+        self.device = None
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.timeout = timeout
+
+        if optional_args is None:
+            optional_args = {}
+
+        self.transport = optional_args.get("transport", "ssh")
+
+        # Netmiko possible arguments
+        netmiko_argument_map = {
+            "port": None,
+            "secret": "",
+            "verbose": False,
+            "keepalive": 30,
+            "global_delay_factor": 1,
+            "use_keys": False,
+            "key_file": None,
+            "ssh_strict": False,
+            "system_host_keys": False,
+            "alt_host_keys": False,
+            "alt_key_file": "",
+            "ssh_config_file": None,
+        }
+
+        # Build dict of any optional Netmiko args
+        self.netmiko_optional_args = {}
+        for k, v in netmiko_argument_map.items():
+            try:
+                self.netmiko_optional_args[k] = optional_args[k]
+            except KeyError:
+                pass
+        self.global_delay_factor = optional_args.get("global_delay_factor", 1)
+
+        # Set the default port if not set
+        default_port = {"ssh": 22}
+        self.netmiko_optional_args.setdefault("port", default_port[self.transport])
+
+    def open(self):
+        """Open connection to the device."""
+        device_type = "fortinet_ssh"
+        self.device = self._netmiko_open(
+            device_type, netmiko_optional_args=self.netmiko_optional_args
+        )
+
+    def close(self):
+        """Close connection to the device."""
+        self.device.disconnect()
